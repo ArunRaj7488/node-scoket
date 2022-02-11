4e80fcec5a5754a5c8e1c94331ce28f282f1ced4
arungautamr <arun.gautam@nixet.tech>
2022-02-11 20:08:33 +0530

    add socket io

--- index.js
+++ index.js
@@ -7,6 +7,26 @@ const cors = require('cors')
 const app = express();
 
 app.use(cors());
+console.log("test server")
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requeted-With, Content-Type, Accept, Authorization, RBR'
+  );
+  if (req.headers.origin) {
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+  }
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    return res.status(200).json({});
+  }
+  next();
+  return;
+});
+
 
 const port = process.env.PORT || 4002;
 
